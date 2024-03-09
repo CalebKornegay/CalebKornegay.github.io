4d5610019279ad96ab7f6528101b26c5eaf41e86
Caleb Kornegay <caleb.kornegay@gmail.com>
2024-03-09 15:39:54 -0500

    make divider longer to fix ref

--- site/components/divider.tsx
+++ site/components/divider.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 
 export default function Divider() {
   return (
-    <motion.div className="bg-gray-200 my-16 h-16 w-1 rounded-full hidden sm:block"
+    <motion.div className="bg-gray-200 my-16 h-24 w-1 rounded-full sm:h-48"
         initial={{opacity: 0, y: 100}}
         animate={{opacity: 1, y: 0}}
         transition={{
