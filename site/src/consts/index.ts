5c08335728664897e7bbea8ed6b292f800dc3b85
CalebKornegay <caleb.kornegay@gmail.com>
2025-06-13 16:12:02 -0400

    Incorrectly named file

--- site/src/consts/index.ts
+++ site/src/consts/index.ts
@@ -39,7 +39,7 @@ export const homelinks: homelink[] = [
 
 export const hotlinks = [
   {
-    resource: "/Resume.pdf",
+    resource: "/resume.pdf",
     icon: TbFileDownload,
     icon_color: "#121212",
   },
